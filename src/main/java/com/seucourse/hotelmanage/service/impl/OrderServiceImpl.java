3bbfae69cac8c70a6ae2579bcdd2d89ac0edc275
qwerty472123 <ebpqwerty472123@gmail.com>
2020-09-04 22:41:40 +0800

    fix order?

--- src/main/java/com/seucourse/hotelmanage/service/impl/OrderServiceImpl.java
+++ src/main/java/com/seucourse/hotelmanage/service/impl/OrderServiceImpl.java
@@ -1,11 +1,14 @@
 package com.seucourse.hotelmanage.service.impl;
 
+import com.seucourse.hotelmanage.entity.Conflict;
 import com.seucourse.hotelmanage.entity.Order;
+import com.seucourse.hotelmanage.mapper.ConflictMapper;
 import com.seucourse.hotelmanage.mapper.OrderMapper;
 import com.seucourse.hotelmanage.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service("orderService")
@@ -13,6 +16,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     OrderMapper orderMapper;
+
+    @Autowired
+    ConflictMapper conflictMapper;
 
     @Override
     public List<Order> queryOrdersByUserId(Integer userId) {
@@ -22,6 +28,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void insertOrder(Order order) {
+        for(long time = order.getStartDate().getTime(); time <= order.getEndDate().getTime(); time += 3600 * 1000 * 24) {
+            conflictMapper.insertConflict(Conflict.builder().roomId(order.getRoomId()).date(new Date(time)).build());
+        }
         orderMapper.insertOrder(order);
     }
 
