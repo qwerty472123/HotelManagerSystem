2237bbed20ef50c17b4fee20f898ab38315a596c
qwerty472123 <ebpqwerty472123@gmail.com>
2020-09-05 09:40:33 +0800

    Revert "ADD EMP module stage 3"
    This reverts commit 8d96ad148ad074829ff8770543d1ada6afc11e68.

--- src/main/java/com/seucourse/hotelmanage/controller/frontEmpController.java
+++
@@ -1,9 +0,0 @@
-package com.seucourse.hotelmanage.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping(path = "/frontemp")
-public class frontEmpController {
-}
